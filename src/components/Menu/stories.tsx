44d51e8f06a5e988bb0157b2a05b4d115a9c29d5
Damn it Rumi <guidepaulasoares@hotmail.com>
2022-11-01 10:51:22 -0300

    Criação do componente Menu

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,12 @@
+import { Menu } from ".";
+import mock from "./mock";
+
+export default {
+  title: "Menu",
+  component: Menu,
+  args: mock,
+};
+
+export const Template = (args) => {
+  return <Menu {...args} />;
+};
