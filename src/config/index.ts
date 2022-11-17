b736a922e54e41e2244a10fa002a451d7aa48f65
Damn it Rumi <guidepaulasoares@hotmail.com>
2022-11-17 02:27:50 -0300

    Correção URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,6 @@ export default {
   url: "https://strapi-landing-pages-project-2.herokuapp.com",
   siteName: "Guilherme Soares",
   defaultSlug: "strapi-blog",
-  graphqlURL: "https://strapi-blog-rumi.herokuapp.com/graphql",
+  // graphqlURL: "http://localhost:1337/graphql",
+  graphqlURL: "http://strapi-blog-rumi.herokuapp.com/graphql",
 };
